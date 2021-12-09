abd89e4b9589e6920f83c69feca96aede095417b
kristinakazlovskaya <kristinagril@icloud.com>
2021-12-10 00:37:33 +0300

    feat: update start page

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const btnPlay = document.getElementById('play');
+let btnPlay = document.querySelector('#playBtn');
 
 let gameJSIsLoaded = false;
 
-document.addEventListener('click', openGame);
+btnPlay.addEventListener('click', openGame);
 
 function openGame() {
   window.location.hash = "game";
@@ -12,10 +12,10 @@ function renderGamePage(data) {
   const container = document.getElementById('mainpage');
   container.innerHTML = data;
   if (!gameJSIsLoaded) {
-    const gameScript = document.createElement('script');
-    gameScript.src = 'app.js';
-    document.body.appendChild(gameScript);
-    gameJSIsLoaded = true;
+  const gameScript = document.createElement('script');
+  gameScript.src = 'app.js';
+  document.body.appendChild(gameScript);
+  gameJSIsLoaded = true;
   }
 };
 
@@ -28,20 +28,20 @@ function updatePage() {
   let hash = window.location.hash.substr(1);
 
   switch (hash) {
-    case "":
-      $.ajax("index.html", {
-          type: "GET",
-          datatype: "html",
-          success: renderIndexPage,
-      });
-      break;
-    case "index":
-      $.ajax(hash + ".html", {
-          type: "GET",
-          datatype: "html",
-          success: renderIndexPage,
-      });
-      break;
+    // case "":
+    //   $.ajax("index.html", {
+    //       type: "GET",
+    //       datatype: "html",
+    //       success: renderIndexPage,
+    //   });
+    //   break;
+    // case "index":
+    //   $.ajax(hash + ".html", {
+    //       type: "GET",
+    //       datatype: "html",
+    //       success: renderIndexPage,
+    //   });
+    //   break;
     case "game":
       $.ajax(hash + ".html", {
           type: "GET",
@@ -52,8 +52,9 @@ function updatePage() {
   }
 };
 
-window.onhashchange = updatePage;
-
-updatePage()
+//window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('hashchange', updatePage);
+  updatePage();
+//});
 
 //rewrite with Fetch
